ad934d9170fe8aeb3899611a38ed5cd9df5e616a
liangyingshi0 <1649020848@qq.com>
2020-05-07 12:41:06 +0800

    update

--- Hadoop/src/Work_1/SVG_job.java
+++ Hadoop/src/Work_1/SVG_job.java
@@ -36,7 +36,7 @@ public class SVG_job {
 			int count = 0;
 			for (SVG_Writable val : values){
 				sum += val.getCount()*val.getAverage();
-				count += 1;
+				count += val.getCount();
 			}
 			result.setCount(count);
 			result.setAverage(sum/count);
@@ -51,7 +51,7 @@ public class SVG_job {
 			int count = 0;
 			for (SVG_Writable val : values){
 				sum += val.getCount()*val.getAverage();
-				count += 1;
+				count += val.getCount();
 			}
 			result.setCount(count);
 			result.setAverage(sum/count);
