def1ca7cf5e0538b5943d0c719e382957340a444
liangyingshi0 <1649020848@qq.com>
2020-05-09 17:43:40 +0800

    update

--- Hadoop/src/Work_1/SVG_job.java
+++ Hadoop/src/Work_1/SVG_job.java
@@ -17,7 +17,8 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 //MapReduce程序
 public class SVG_job {
-
+	
+	//Mapper
 	public static class SVGMap extends Mapper<LongWritable, Text, IntWritable, SVG_Writable>{
 		private SVG_Writable w = new SVG_Writable();
 		protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
@@ -25,25 +26,11 @@ public class SVG_job {
 			w.setAverage(Integer.parseInt(value.toString().split(",")[1]));
 			context.write(new IntWritable(1), w);
 			//new IntWritable(1)是新建了这个类的一个对象，而数值道1这是参数。在Hadoop中它相当于java中Integer整型变量，为这个变量赋值为1.
-			//输出<1,{w}>    结果：1 53 88     意思：53个数，平均值为88
+			//输出<1,{w}>    若最终在reducer输出这个形式，则结果：1 53 88     意思：53个数，平均值为88
 		}
 	}
 	
-	public static class SVGReduce extends Reducer<IntWritable, SVG_Writable, NullWritable, IntWritable>{
-		private SVG_Writable result = new SVG_Writable();
-		protected void reduce(IntWritable key, Iterable<SVG_Writable> values, Context context) throws IOException, InterruptedException{
-			int sum = 0;
-			int count = 0;
-			for (SVG_Writable val : values){
-				sum += val.getCount()*val.getAverage();
-				count += val.getCount();
-			}
-			result.setCount(count);
-			result.setAverage(sum/count);
-			context.write(NullWritable.get(), new IntWritable(sum/count));
-		}
-	}
-	
+	//Combiner
 	public static class SVGCombine extends Reducer<IntWritable, SVG_Writable, IntWritable, SVG_Writable>{
 		private SVG_Writable result = new SVG_Writable();
 		protected void reduce(IntWritable key, Iterable<SVG_Writable> values, Context context) throws IOException, InterruptedException{
@@ -60,7 +47,24 @@ public class SVG_job {
 		}
 	}
 	
+	//Reducer
+	public static class SVGReduce extends Reducer<IntWritable, SVG_Writable, NullWritable, IntWritable>{
+		private SVG_Writable result = new SVG_Writable();
+		protected void reduce(IntWritable key, Iterable<SVG_Writable> values, Context context) throws IOException, InterruptedException{
+			int sum = 0;
+			int count = 0;
+			for (SVG_Writable val : values){
+				sum += val.getCount()*val.getAverage();
+				count += val.getCount();
+			}
+			result.setCount(count);
+			result.setAverage(sum/count);
+			context.write(NullWritable.get(), new IntWritable(sum/count));
+		}
+	}
+	
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+		//运行前，要先把svg_output文件夹删去，否则会报错；两个数据文件要放在svg_input文件夹里
 		args = new String[]{"src/Work_1/svg_input","src/Work_1/svg_output"};
 		Configuration conf = new Configuration();
 		Job job = Job.getInstance(conf);
