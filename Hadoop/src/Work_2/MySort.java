99b9feb67b5d23783f089f73690876dd5f61cfbc
liangyingshi0 <1649020848@qq.com>
2020-05-16 18:53:13 +0800

    自定义排序

---
+++ Hadoop/src/Work_2/MySort.java
@@ -0,0 +1,22 @@
+package Work_2;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+
+public class MySort extends WritableComparator{
+	//自定义排序
+	public MySort() {
+		super(IntWritable.class, true);
+	}
+
+	@Override
+	public int compare(WritableComparable a, WritableComparable b) {
+		//提取月份比较
+		IntWritable V1 = new IntWritable(Integer.parseInt(a.toString().split("-")[1]));
+		IntWritable V2 = new IntWritable(Integer.parseInt(b.toString().split("-")[1]));
+		return V2.compareTo(V1);
+	}
+
+}
