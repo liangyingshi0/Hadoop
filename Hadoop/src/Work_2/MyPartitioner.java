5ee99d846e409e9091f3209e726b122ab2be01ce
liangyingshi0 <1649020848@qq.com>
2020-05-17 10:50:50 +0800

    自定义分区

--- Hadoop/src/Work_2/MyPartitioner.java
+++ Hadoop/src/Work_2/MyPartitioner.java
@@ -7,8 +7,8 @@ public class MyPartitioner extends Partitioner<Text, MinMaxWritable>{
 	//自定义分区，2017年一个区，2018年一个区
 	@Override
 	public int getPartition(Text key, MinMaxWritable value, int numPartitions) {
-		int year = Integer.parseInt(key.toString().split("-")[0]);
-		if(year == 2017){
+		String year = key.toString().split("-")[0];
+		if(year.equals("2017")){
 			return 0;
 		}
 		else
