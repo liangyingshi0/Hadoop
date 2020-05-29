ffcfa02e46ad13560b701ab2e6e8c1ae88411b01
liangyingshi0 <1649020848@qq.com>
2020-05-29 22:44:47 +0800

    在MySort文件里，新添加了一个赋值方法

--- Hadoop/src/Work_2/MySort.java
+++ Hadoop/src/Work_2/MySort.java
@@ -14,9 +14,17 @@ public class MySort extends WritableComparator{
 
 	public int compare(WritableComparable a, WritableComparable b) {
 		//提取月份比较
-		IntWritable V1 = new IntWritable(Integer.parseInt(a.toString().split("-")[1]));
-		IntWritable V2 = new IntWritable(Integer.parseInt(b.toString().split("-")[1]));
-		return V2.compareTo(V1);
+		//赋值方法1：
+//		IntWritable V1 = new IntWritable(Integer.parseInt(a.toString().split("-")[1]));
+//		IntWritable V2 = new IntWritable(Integer.parseInt(b.toString().split("-")[1]));
+//		return V2.compareTo(V1);
+		
+		//赋值方法2：
+		IntWritable v1 = new IntWritable();
+		IntWritable v2 = new IntWritable();
+		v1.set(Integer.parseInt(a.toString().split("-")[1]));
+		v2.set(Integer.parseInt(b.toString().split("-")[1]));
+		return v2.compareTo(v1);
 	}
 
 }
